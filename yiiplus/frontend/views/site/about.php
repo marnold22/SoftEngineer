4955d5530c9eee17486340a44b3350860a48f698
marnold22 <minimearnold@gmail.com>
2016-04-26 15:29:27 -0700

    Initial Commit of Project

---
+++ yiiplus/frontend/views/site/about.php
@@ -0,0 +1,23 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <p>
+        Mission:
+        <br></br>
+
+		<ul>
+			<li>Provide an efficient means for researchers with analytical needs to contribute data to their specific biological field of interest.</li>
+			<li>Provide opportunities for students and faculty to contribute their bioinformatics analysis skills to real world biological research projects.</li>
+		</ul>
+		We want users to be able to take part in the research community, so as a reuslt this site is dedicated to the inspiration of learning and pursuing after greater knowledge.  Here you can contribute to the community by helping out fellow researchers with some of thier problems. Whether you are a seasoned vet or a brand new researcher this is a place to get started. There are many different research topics to choose from and if you have more ideas let us know! We hope you find a topic to your liking, and goodluck with your research.
+    </p>
+    <code><?= __FILE__ ?></code>
+</div>
---
+++ yiiplus/frontend/views/site/projects.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Projects';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="projects">
+
+</div>
+
