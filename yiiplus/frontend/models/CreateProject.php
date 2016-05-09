992baf809056c0a71c260f3bf76f7a8161c26879
andrewdodge16 <andrewdodge16@my.whitworth.edu>
2016-05-09 17:28:01 -0400

    added createProject files
    added createProject files in views and models

--- yiiplus/frontend/models/CreateProject.php
+++ yiiplus/frontend/models/CreateProject.php
@@ -9,25 +9,47 @@ use Yii;
 
 class CreateProject extends Model
 {
-	public $creatorName;
+	public $username;
 	public $creatorAssociation;
 	public $projectName;
 	public $projectDescription;
 	public $displayImage;
 	public $displayDoc;
-	public $requestEmail;
-	public $requestEmailRepeat;
+	public $email;
 
 	public function rules()
 	{
 		return [
-            [['displayImage'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg, jpeg', 'maxFiles' => 4],
-            [['displayDoc'], 'file', 'skipOnEmpty' => true, 'extensions' => 'docx, xlsx, txt, rtf, csv', 'maxFiles' => 4],
-            [['requestEmail'], 'email', 'allowEmpty' => false],
-            [['creatorName'], 'type', ]
+            [['displayImage'], 'file', 'extensions' => 'png, jpg, jpeg', 'maxFiles' => 4],
+            [['displayDoc'], 'file', 'extensions' => 'docx, xlsx, txt, rtf, csv', 'maxFiles' => 4],
+            [['requestEmail'], 'email', 'required'],
+            [['creatorAssociation'], 'string', 'max' => 150], 
+            [['projectDescription'], 'string'], 
+            [['projectName'], 'unique', 'required'],
+            [['requestEmailRepeat'], 'email', 'allowEmpty' => false],
         ];
 
 	}
+	public function makeProject()
+    {
+        if ($this->validate()) {
+            foreach ($this->displayImage as $file) {
+                $file->saveAs(Yii::$app->basePath.'/web/projects/displayImages/' . $file->baseName . '.' . $file->extension);
+            }
+            foreach ($this->displayDoc as $file) {
+                $file->saveAs(Yii::$app->basePath.'/web/projects/displayDocs/' . $file->baseName . '.' . $file->extension);
+            }
+        	$project->creatorAssociation = $this->creatorAssociation;
+        	$project->projectDescription = $this->projectDescription;
+        	$project->projectName = $this->projectName;
+            return true;
+        } 
+        else {
+            return false;
+        }
+        
+
+    }
 }
 
 
